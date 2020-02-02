0c9ab4b7ebbc9f9cf2c29ab0c1cdd4060d397a9d
Traci P. DuBose <TraciPopejoy@ou.edu>
2020-02-02 14:28:38 -0600

    got landuse, nhd, flowdata, and air temp data

--- MacroecoDrivers.R
+++ MacroecoDrivers.R
@@ -1,72 +1,220 @@
-devtools::install_github("USGS-R/EflowStats")
+library(lubridate);library(dataRetrieval);library(EflowStats)
 
 #need to build site x environment dataframe
 head(SiteID) #these are my sites
 #want to find oldest and youngest year at each site
-Site_data<-AxL %>% group_by(Species, Site) %>%
+Site.data<-AxL %>% group_by(Species, Site) %>%
+  rename(SiteID=Site)%>%
   summarize(maxYear=max(Year),
             minYear=min(Year)) %>% 
-  left_join(SiteID) %>%
-  dplyr::select()
-
-library(dataRetrieval)
+  left_join(SiteID, by="SiteID") %>%
+  dplyr::select(Species,Site.Agg, HUC.8,HUC.12,USGS.Gage, maxYear, minYear, Lat.cor, Long.cor) %>%
+  filter(!duplicated(Lat.cor))%>%
+  rowwise() %>%
+  mutate(d.av.begin=min(whatNWISdata(siteNumber=USGS.Gage, parameterCd="00060", service="dv")$begin_date),
+         prob.year=minYear-year(d.av.begin)) %>%
+  arrange(prob.year)
+whatNWISdata(siteNumber="05586100", parameterCd="00060", service="dv")
 
 #use Eflow to pull out some hydrological variables -----
-library(EflowStats)
-dailyQ <- readNWISdata(siteNumber=SiteID$`USGS gage Identifier`[1],
-                       parameterCd="00060",
-                       startDate="",
-                       endDate="")
-dailyQClean <- validate_data(dailyQ[c("Date","X_00060_00003")],yearType="water")
+water.stats<-NULL
+get_dis_stats<- function(data,site,gage, maxyear, shellyear, gageyear) {
+  dailyQ<-readNWISdata(siteNumber=gage,
+                      parameterCd="00060",
+                      startDate=paste(max(shellyear, gageyear+1),"10-01", sep="-"),
+                      endDate=paste(maxyear,"09-30", sep="-"),
+                      service="dv") %>%
+  mutate(Date=as.Date(dateTime))
+  dailyQClean <- validate_data(dailyQ[c("Date","X_00060_00003")],
+                               yearType="water") 
+  siteInfo <- readNWISsite(siteNumber = gage)
+  drainageArea <- siteInfo$drain_area_va #get drainage area
 
-
-
-#Get drainage area
-siteInfo <- readNWISsite(siteNumber = "04085427")
-drainageArea <- siteInfo$drain_area_va
-
-#Get peak flows
-peakFlows <- readNWISpeak(siteNumber = "04085427",
-                          startDate = "2000-10-01",
-                          endDate = "2012-9-30")
-
-#Check data for completeness
-
-dailyQClean <- validate_data(dailyQ[c("Date", "X_00060_00003")], yearType="water")
-
-#Get flood recurence threshold
-floodThresh <- get_peakThreshold(dailyQClean[c("date","discharge")],
-                                peakFlows[c("peak_dt","peak_va")])
-
-#Calculate all hit stats
-calc_allHITOut <- calc_allHIT(dailyQClean,
+  #Get peak flows
+  peakFlows <- readNWISpeak(siteNumber = gage,
+                            startDate=paste(max(shellyear, gageyear+1),"10-01", sep="-"),
+                            endDate=paste(maxyear,"09-30", sep="-"))
+  #Get flood recurence threshold
+  floodThresh <- get_peakThreshold(dailyQClean[c("date","discharge")],
+                                  peakFlows[c("peak_dt","peak_va")])
+  #Calculate all hit stats
+  #get a lot of warnings, but its just a slight function language change,
+  #functionality is the same (changed . to _)
+  calc_allHITOut <- calc_allHIT(dailyQClean,
                              drainArea=drainageArea,
-                              floodThreshold=floodThresh)
+                              floodThreshold=floodThresh) %>%
+  filter(indice %in% c("ml17")) %>%
+  mutate(Site.Agg=site)
+  return(calc_allHITOut)
+}
 
-# figure out water temperature -----
+get_dis_stats(site="test",gage="05341550",maxyear=2017,shellyear=1986,gageyear=2011)
 
+site.dis<-NULL
+for(r in c(1:3,5:14,16:29:31:40)){
+  dis.t<-get_dis_stats(site=Site.data$Site.Agg[r],
+                       gage=Site.data$USGS.Gage[r],
+                       maxyear=Site.data$maxYear[r],
+                       shellyear=Site.data$minYear[r],
+                       gageyear=year(Site.data$d.av.begin[r])) 
+  site.dis<-bind_rows(site.dis, dis.t)
+}
 
+#problem gages with missing data;
+#will manipulate data and get HIT values
+#might do chunks of years and then average
+Site.data[4,] #doesn't work because gage problem in 1991 
+Site.data[15,] #missing 1/24/2017 womp womp
+Site.data[30,] #haven't investigated yet
+  
+# figure out stream characteristics -----
+library(nhdplusTools); library(sf)
 
-install.packages("nhdplusTools")
-library(nhdplusTools)
-library(sf)
-
-start_point <- st_sfc(st_point(c(-89.362239, 43.090266)), crs = 4269)
+start_point <- st_sfc(st_point(c(Site.data$Long.cor[1], 
+                                 Site.data$Lat.cor[1])), crs = 4269)
 start_comid <- discover_nhdplus_id(start_point)
 
-flowline <- navigate_nldi(list(featureSource = "comid", 
-                               featureID = start_comid), 
-                          mode = "upstreamTributaries", 
-                          data_source = "")
-
 subset_file <- tempfile(fileext = ".gpkg")
-subset <- subset_nhdplus(comids = flowline$nhdplus_comid,
+subset <- subset_nhdplus(comids = start_comid,
                          output_file = subset_file,
                          nhdplus_data = "download", 
                          return_data = TRUE)
-flowline <- subset$NHDFlowline_Network
-catchment <- subset$CatchmentSP
-waterbody <- subset$NHDWaterbody
 
+nhdata <- subset$NHDFlowline_Network
+catchment <- subset$CatchmentSP
+waterbody <- subset$NHDWaterbody 
+#figuring out stream temperatur
+
+dailyT.all<-NULL
+for(k in 1:40){
+dailyT <- readNWISdata(siteNumber=Site.data$USGS.Gage[k], service="dv",
+                parameterCd="00010",
+                startDate=paste(Site.data$minYear-1,"10-01", sep="-")[k],
+                endDate=paste(Site.data$maxYear,"09-30", sep="-")[k]) %>%
+  mutate(Site.Agg=Site.data$Site.Agg[k],
+         Species=Site.data$Species[k])
+dailyT.all<-bind_rows(dailyT.all, dailyT)
+}
+
+dailyT.all %>% group_by(site_no, Site.Agg, Species) %>%
+  summarize(meanT=mean(X_00010_00003, na.rm=T),
+            nobs=n())
+
+# land use data ====
+#tutorial: https://mbjoseph.github.io/posts/2018-12-27-categorical-spatial-data-extraction-around-buffered-points-in-r/
+library(raster); library(FedData); library(rgdal)
+
+landprop.sum<-NULL
+for(m in 1:28){
+  Sp.Site.data<-Site.data %>% ungroup() %>%
+    dplyr::select(Site.Agg, Lat.cor, Long.cor) %>%
+    filter(!duplicated(.)) %>%
+    slice(m)
+  print(Sp.Site.data$Site.Agg)
+  coordinates(Sp.Site.data) <- ~Long.cor + Lat.cor
+  proj4string(Sp.Site.data) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+  
+  #download land cover data; takes a while (~30s per tile)
+  buff_site<-buffer(Sp.Site.data, width=100)
+  nlcd_raster <- get_nlcd(buff_site,
+                          label=paste(Sp.Site.data$Site.Agg),
+                          dataset="landcover")
+  
+  # reproject your data to raster's crs
+  buff_site <- spTransform(buff_site, projection(nlcd_raster))
+  
+  landcover <- extract(nlcd_raster, buff_site)
+  landprop<-as.data.frame(prop.table(table(landcover))) %>% 
+    cbind(Site.Agg=Sp.Site.data$Site.Agg)
+  landprop.sum<-bind_rows(landprop.sum, landprop)
+}
+site.landuse <- landprop.sum %>%
+  mutate(ID=as.numeric(landcover)) %>%
+  left_join(nlcd_raster@data@attributes[[1]][,c(1,8)]) 
+site.landuse %>% group_by(Site.Agg) %>% 
+  summarize(sumF=sum(Freq)) %>%
+  arrange(sumF)
+site.forest<-site.landuse %>% 
+  dplyr::filter(NLCD.2011.Land.Cover.Class %in%
+                unique(NLCD.2011.Land.Cover.Class)[c(5,7,14)]) %>%
+  mutate(Prop=Freq*100)
+
+# combine into site x environment table -----
+
+# apply multiple regression to estimate a & b for temp regression
+
+# pull met data down
+Site.data.met<-Site.data %>%
+  mutate(nMonths=(maxYear-minYear)*12) %>%
+  group_by(HUC.8) %>% 
+  arrange(desc(minYear)) %>% 
+  slice(1)
+sum(Site.data.met$nMonths)
+
+library(rnoaa)
+options("noaakey" = Sys.getenv("noaakey"))
+#ncdc_locs_cats(datasetid="GSOY")
+#ncdc_datatypes(datasetid = "GSOY")
+for(u in 1:14){
+  y.span<-seq.Date(as.Date(paste(Site.data.met$maxYear[u], "-01-01", sep="")),
+                   as.Date(paste(Site.data.met$minYear[u], "-01-01", sep="")),
+                   by="-9 years")
+  y.span <-c(y.span, as.Date(paste(Site.data.met$minYear[u], "-01-01", sep="")))
+  ly<-length(y.span)
+  out_all<-NULL
+  for(l in ly:2){
+      out <- ncdc(datasetid='GSOY', 
+                  locationid=paste('HUC:', Site.data.met$HUC.8[u], sep=""), 
+                  datatypeid=c('TAVG','TMAX','TMIN'), 
+                  startdate =paste(y.span[l]),
+                  enddate=paste(y.span[l-1]),
+                  limit=1000)
+      out_all<-rbind(out_all, out$data) 
+    }
+    
+    print(paste(Site.data.met$HUC.8[u],paste(u, l)))
+    write.csv(out_all, file=paste("data/weather/",
+                                paste(Site.data.met$HUC.8[u],
+                                      ".csv", sep = ""), sep=""))
+  
+}
+#problem childs: 03160107
+which(is.na(match(Site.data.met$HUC.8,weather$HUC.8)))
+Site.data.met[1,]
+
+wfiles<-paste("data/weather/",
+               list.files(path="data/weather/"), sep="")
+weather<-NULL
+for (i in 1:length(wfiles)){
+  ws <- read.csv(wfiles[i]) %>% 
+    mutate(HUC.8=substr(wfiles[i],14,21),
+           Date.r=as.Date(date))
+  weather<-rbind(weather, ws)
+} 
+pdf('weathercheck.pdf')
+ggplot(weather[weather$datatype=="TAVG",], aes(x=Date.r,y=value))+
+    geom_point()+
+    scale_x_date(date_labels="%y")+
+    theme(axis.text.x = element_text(angle=30))+
+  facet_wrap(~HUC.8, scales="free")
+ggplot(weather[weather$datatype=="TMIN",], aes(x=Date.r,y=value))+
+  geom_point()+
+  scale_x_date(date_labels="%y")+
+  theme(axis.text.x = element_text(angle=30))+
+  facet_wrap(~HUC.8, scales="free")
+ggplot(weather[weather$datatype=="TMAX",], aes(x=Date.r,y=value))+
+  geom_point()+
+  scale_x_date(date_labels="%y")+
+  theme(axis.text.x = element_text(angle=30))+
+  facet_wrap(~HUC.8, scales="free")
+dev.off()
+
+# apply regression to calculate water temperature
+
+# calculate summary statistics for water temperature
 
 #
+
+
+
+
