6f1a62c4b2ae7fd2e093f5ccebec68fa05de651a
Traci P. DuBose <TraciPopejoy@ou.edu>
2020-02-06 16:22:11 -0600

    explored which models to discuss

--- ModelComparison.R
+++ ModelComparison.R
@@ -4,7 +4,7 @@ site.hyd<-read.csv('data/HITDischarge.csv') %>% dplyr::select(-X) %>%
   mutate(Sp=case_when(Sp %in% c("LCAR","LORN")~"LAMP",
                Sp =="APLI"~"APLI"))
 site.schar<-read.csv('data/SiteChar.csv')%>% dplyr::select(-X)
-site.imp.landuse<-read.csv('data/LanduseT.csv')
+site.imp.landuse<-read.csv('data/LanduseT.csv')%>% dplyr::select(-X)
 w.sum<-read.csv('data/weatherSum.csv')%>% dplyr::select(-X)
 
 env.model.data<-Site.data %>% ungroup() %>%
@@ -24,32 +24,52 @@ env.model.data<-Site.data %>% ungroup() %>%
          summerHflow=mean(c(mh5,mh6,mh7,mh8)))
 
 em.data<-env.model.data %>% ungroup() %>%
-  dplyr::select(x50,SpF,Lat.cor,streamorde, Xwtavg, Xwtmax,
-                ma2, fl1,fh1,dl16,dl17,ra8,summerLflow,summerHflow)
+  mutate(SpN=factor(SpF),
+         Lat.round=round(Lat.cor, 2))%>%
+  dplyr::select(-SpF, -Lat.cor, -mu_l_z, -HUC.8,-gnis_name,-miny,-maxy,
+                -Site.Agg, -Long.cor,-totdasqkm, -lengthkm,-nobs, -Latscale,
+                -temp0001, -Xwtmax, -Xwtmin,-elev)
 
-em.data<-as.data.frame(em.data)
+bf.allsing<-generalTestBF(x50~.*SpN, data=em.data, 
+                          whichModels = 'bottom')
+    
+head(bf.allsing)
+tail(bf.allsing)
+
+ds<-head(bf.allsing, n=10)/bf.allsing["SpN"]
+plot(ds)
+
 # Model comparison ------------
-cor.test(y=em.data$x50, x=em.data$Lat.cor)
-correlationBF(y = em.data$x50, x = em.data$Lat.cor)
+bf<-generalTestBF(x50~., data=em.data,
+                  progress=T, neverExclude = "SpN") #341
+length(bf)
+head(bf, n=10)
 
-bf.Lat <- regressionBF(x50 ~ Lat.cor, data = em.data)
-bf.Sp <- regressionBF(x50 ~ SpF, data = em.data)
-bf.streamorde <- regressionBF(x50 ~ streamorde, data = em.data)
-bf.Xwtavg <- regressionBF(x50 ~ Xwtavg, data = em.data)
-bf.Xwtmax <- regressionBF(x50 ~ Xwtmax, data = em.data)
-bf.summerLflow <- regressionBF(x50 ~ summerLflow, data = em.data)
-bf.summerHflow <- regressionBF(x50 ~ summerHflow, data = em.data)
+head(bf)/bf["SpN"]
+
+
+me.hyp<-head(bf)/bf["Lat.cor"]
+plot(me.hyp)
+
+bf.sp<- regressionBF(x50~ . , data=em.data[,1:4])
+
+bf.Lat <- regressionBF(x50 ~ Lat.round,  data=env.model.data)
+bf.Sp <- regressionBF(x50 ~ SpF, data = env.model.data)
+bf.streamorde <- regressionBF(x50 ~ streamorde, data = env.model.data)
+bf.Xwtavg <- regressionBF(x50 ~ Xwtavg, data = env.model.data)
+bf.Xwtmax <- regressionBF(x50 ~ Xwtmax, data = env.model.data)
+bf.summerLflow <- regressionBF(x50 ~ summerLflow, data = env.model.data)
+bf.summerHflow <- regressionBF(x50 ~ summerHflow, data = env.model.data)
 
 bf.all<-c(bf.Lat,bf.Sp,bf.streamorde,bf.Xwtavg,bf.Xwtmax,
           bf.summerLflow, bf.summerHflow)
 length(bf.all)
-bf.all['Lat.cor']
+bf.all['Lat.round']
 head(bf.all)
 ## Compare the 5 best models to the best
 bf2 = head(bf.all) / max(bf.all)
 bf2
 plot(bf2)
-
 
 chains = posterior(complaintsLearningBf, iterations = 10000)
 summary(chains)
